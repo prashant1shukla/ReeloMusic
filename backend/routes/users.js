3b2423211e509957e5e310c9f61329d2119854dd
prashant1shukla <77752155+prashant1shukla@users.noreply.github.com>
2024-08-18 02:13:02 +0530

    Made Middleware for auth, made more  user APIs

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 const { User, validate } = require("../models/user");
 const bcrypt = require("bcrypt");
-// const admin = require("../middleware/admin");
-// const auth = require("../middleware/auth");
-// const validateObjectId = require("../middleware/validateObjectId");
+const admin = require("../middleware/admin");
+const auth = require("../middleware/auth");
+const validateObjectId = require("../middleware/validateObjectId");
 
 // create user
 router.post("/", async (req, res) => {
@@ -30,32 +30,32 @@ router.post("/", async (req, res) => {
         .send({ data: newUser, message: "Account created successfully" });
 });
 
-// // get all users
-// router.get("/", admin, async (req, res) => {
-//     const users = await User.find().select("-password -__v");
-//     res.status(200).send({ data: users });
-// });
+// get all users
+router.get("/", admin, async (req, res) => {
+    const users = await User.find().select("-password -__v");
+    res.status(200).send({ data: users });
+});
 
-// // get user by id
-// router.get("/:id", [validateObjectId, auth], async (req, res) => {
-//     const user = await User.findById(req.params.id).select("-password -__v");
-//     res.status(200).send({ data: user });
-// });
+// get user by id
+router.get("/:id", [validateObjectId, auth], async (req, res) => {
+    const user = await User.findById(req.params.id).select("-password -__v");
+    res.status(200).send({ data: user });
+});
 
-// // update user by id
-// router.put("/:id", [validateObjectId, auth], async (req, res) => {
-//     const user = await User.findByIdAndUpdate(
-//         req.params.id,
-//         { $set: req.body },
-//         { new: true }
-//     ).select("-password -__v");
-//     res.status(200).send({ data: user, message: "Profile updated successfully" });
-// });
+// update user by id
+router.put("/:id", [validateObjectId, auth], async (req, res) => {
+    const user = await User.findByIdAndUpdate(
+        req.params.id,
+        { $set: req.body },
+        { new: true }
+    ).select("-password -__v");
+    res.status(200).send({ data: user, message: "Profile updated successfully" });
+});
 
-// // delete user by id
-// router.delete("/:id", [validateObjectId, admin], async (req, res) => {
-//     await User.findByIdAndDelete(req.params.id);
-//     res.status(200).send({ message: "Successfully deleted user." });
-// });
+// delete user by id
+router.delete("/:id", [validateObjectId, admin], async (req, res) => {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).send({ message: "Successfully deleted user." });
+});
 
 module.exports = router;
