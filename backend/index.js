a9315cb69361b4c6261725d7c5b323110e1815ad
prashant1shukla <77752155+prashant1shukla@users.noreply.github.com>
2024-08-18 02:46:45 +0530

    Made Playlist APIs

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ const express= require("express");
 const connection = require("./db");
 const userRoutes=require("./routes/users");
 const authRoutes=require("./routes/auth");
+const songRoutes=require("./routes/songs");
+const playistRoutes=require("./routes/playlist");
 const app=express();
 const cors=require("cors");
 
@@ -13,6 +15,8 @@ app.use(express.json());
 
 app.use("/api/users",userRoutes);
 app.use("/api/login",authRoutes);
+app.use("/api/songs",songRoutes);
+app.use("/api/playlists",playistRoutes);
 
 const port= process.env.PORT || 8080;
 app.listen(port, console.log(`Listening on port ${port}...`))  
